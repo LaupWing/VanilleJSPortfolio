571964101a7b5f7a83fb384e20f73ab396dff9c5
LaupWing <loc_strict@hotmail.com>
2020-05-22 12:11:10 +0200

    route finished

---
+++ src/PageHandlers/Route.ts
@@ -0,0 +1,10 @@
+export default class Route{
+    constructor(
+        public name:string,
+        public htmlName: string,
+        public defaultRoute: boolean,
+    ){}
+    router(hashedPath: string):boolean{  
+        return hashedPath.replace('#', '') === this.name;
+    }
+}
--- src/PageHandlers/Router.ts
+++
@@ -1,8 +0,0 @@
-export default class Router{
-    constructor(){
-        window.addEventListener('hashchange', this.router)
-    }
-    router():void{
-        
-    }
-}
