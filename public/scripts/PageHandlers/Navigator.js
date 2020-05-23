9b91021d2db4321b6f0ba251ab7607dea68fe45c
LaupWing <loc_strict@hotmail.com>
2020-05-23 18:53:40 +0200

    it finally works'

--- public/scripts/PageHandlers/Navigator.js
+++
@@ -1,9 +0,0 @@
-var Router = /** @class */ (function () {
-    function Router() {
-        window.addEventListener('hashchange', this.router);
-    }
-    Router.prototype.router = function () {
-    };
-    return Router;
-}());
-export default Router;
