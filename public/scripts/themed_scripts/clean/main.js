eb5cab711e02ae3a4fc06ba764314f716a917d99
LaupWing <loc_strict@hotmail.com>
2020-05-23 20:44:10 +0200

    added clean

--- public/scripts/themed_scripts/clean/main.js
+++ public/scripts/themed_scripts/clean/main.js
@@ -1,5 +1,13 @@
 export default class Clean {
     constructor() {
         this.links = document.querySelectorAll('#clean nav a');
+        this.links.forEach(link => {
+            link.addEventListener('click', this.handleLink);
+        });
+    }
+    handleLink(e) {
+        e.preventDefault();
+        window.location.href = e.target.href;
+        console.log(e.target.href);
     }
 }
--- src/themed_scripts/clean/main.ts
+++ src/themed_scripts/clean/main.ts
@@ -2,5 +2,13 @@ export default class Clean{
     links: NodeListOf<HTMLLinkElement>;
     constructor(){
         this.links = document.querySelectorAll('#clean nav a');
+        this.links.forEach(link=>{
+            link.addEventListener('click', this.handleLink);
+        })
+    }
+    handleLink(e:Event){
+        e.preventDefault();
+        window.location.href = (e.target as HTMLLinkElement).href;
+        console.log((e.target as HTMLLinkElement).href);
     }
 }
