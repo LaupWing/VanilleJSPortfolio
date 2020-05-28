c705bfabc364e9f4650edc4c65429ff6d4119686
LaupWing <loc_strict@hotmail.com>
2020-05-28 19:06:43 +0200

    added perserve3d

--- public/scripts/themed_scripts/threed/main.js
+++ public/scripts/themed_scripts/threed/main.js
@@ -7,5 +7,11 @@ export default class ThreeD extends Theme {
         const projects = document.querySelectorAll('.project');
         const activeProject = projects[0];
         activeProject.classList.add('active');
+        document.addEventListener('mousemove', (e) => {
+            console.log('remove');
+            const ax = window.innerWidth / 2 - e.pageX / 20;
+            const ay = window.innerHeight / 2 - e.pageY / 10;
+            activeProject.style.transform = "rotateY(" + ax + "deg) rotateX(" + ay + "deg);";
+        });
     }
 }
