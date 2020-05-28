27f2600696007107780c13602aac49660631bd62
LaupWing <loc_strict@hotmail.com>
2020-05-28 19:19:48 +0200

    done for today

--- public/scripts/themed_scripts/threed/main.js
+++ public/scripts/themed_scripts/threed/main.js
@@ -8,10 +8,9 @@ export default class ThreeD extends Theme {
         const activeProject = projects[0];
         activeProject.classList.add('active');
         document.addEventListener('mousemove', (e) => {
-            console.log('remove');
-            const ax = window.innerWidth / 2 - e.pageX / 20;
-            const ay = window.innerHeight / 2 - e.pageY / 10;
-            activeProject.style.transform = "rotateY(" + ax + "deg) rotateX(" + ay + "deg);";
+            const ax = -(window.innerWidth / 2 - e.pageX) / 20;
+            const ay = (window.innerHeight / 2 - e.pageY) / 10;
+            activeProject.style.transform = "rotateY(" + ax + "deg) rotateX(" + ay + "deg)";
         });
     }
 }
