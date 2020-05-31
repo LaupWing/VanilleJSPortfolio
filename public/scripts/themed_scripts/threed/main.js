5906578a369ce80e8e663eb2d7ac9c750f4c993d
LaupWing <loc_strict@hotmail.com>
2020-06-01 00:37:54 +0200

    update

--- public/scripts/themed_scripts/threed/main.js
+++ public/scripts/themed_scripts/threed/main.js
@@ -1,4 +1,5 @@
 import Theme from '../Theme.js';
+import getProminentColor from '../../utils/getProminentColor.js';
 export default class ThreeD extends Theme {
     constructor() {
         super();
@@ -7,19 +8,22 @@ export default class ThreeD extends Theme {
         const projects = document.querySelectorAll('.project');
         const activeProject = projects[0];
         activeProject.classList.add('active');
-        // const move = (e:MouseEvent)=>{
-        //     const ax = -(window.innerWidth/2- e.pageX)/20;
-        //     const ay = (window.innerHeight/2- e.pageY)/10;
-        //     activeProject.style.transform = "rotateY("+ax+"deg) rotateX("+ay+"deg)"
-        // }
-        // const color = getProminentColor(activeProject.querySelector('img')!);
-        // console.log(color);
-        // document.body.style.background = `rgb(${color.r},${color.g},${color.b})`
-        // this.listeners.push({
-        //    element: document,
-        //    type: 'mousemove' ,
-        //    referenceFunction: move 
-        // });
-        // document.addEventListener('mousemove', move);
+        const move = (e) => {
+            const ax = -(window.innerWidth / 2 - e.pageX) / 20;
+            const ay = (window.innerHeight / 2 - e.pageY) / 10;
+            activeProject.style.transform = "rotateY(" + ax + "deg) rotateX(" + ay + "deg)";
+        };
+        const description = activeProject.querySelector('.project-info p');
+        description === null || description === void 0 ? void 0 : description.addEventListener('animationend', () => {
+            const color = getProminentColor(activeProject.querySelector('img'));
+            console.log(color);
+            document.body.style.background = `rgb(${color.r},${color.g},${color.b})`;
+            this.listeners.push({
+                element: document,
+                type: 'mousemove',
+                referenceFunction: move
+            });
+            document.addEventListener('mousemove', move);
+        });
     }
 }
--- src/themed_scripts/threed/main.ts
+++ src/themed_scripts/threed/main.ts
@@ -10,21 +10,24 @@ export default class ThreeD extends Theme{
         const activeProject = projects[0];
         activeProject.classList.add('active');
         
-        // const move = (e:MouseEvent)=>{
-        //     const ax = -(window.innerWidth/2- e.pageX)/20;
-        //     const ay = (window.innerHeight/2- e.pageY)/10;
-        //     activeProject.style.transform = "rotateY("+ax+"deg) rotateX("+ay+"deg)"
-        // }
+        const move = (e:MouseEvent)=>{
+            const ax = -(window.innerWidth/2- e.pageX)/20;
+            const ay = (window.innerHeight/2- e.pageY)/10;
+            activeProject.style.transform = "rotateY("+ax+"deg) rotateX("+ay+"deg)"
+        }
+        const description = activeProject.querySelector('.project-info p');
+        description?.addEventListener('animationend', ()=>{
+            const color = getProminentColor(activeProject.querySelector('img')!);
+            console.log(color);
+            document.body.style.background = `rgb(${color.r},${color.g},${color.b})`
+            this.listeners.push({
+               element: document,
+               type: 'mousemove' ,
+               referenceFunction: move 
+            });
+    
+            document.addEventListener('mousemove', move);
+        })
 
-        // const color = getProminentColor(activeProject.querySelector('img')!);
-        // console.log(color);
-        // document.body.style.background = `rgb(${color.r},${color.g},${color.b})`
-        // this.listeners.push({
-        //    element: document,
-        //    type: 'mousemove' ,
-        //    referenceFunction: move 
-        // });
-
-        // document.addEventListener('mousemove', move);
     }
 }
