fefbe1f7bd00127f97c6eb5ac549aa5e5eb43b52
LaupWing <loc_strict@hotmail.com>
2020-06-01 11:35:35 +0200

    added buttons for next and previosu

--- public/scripts/themed_scripts/threed/main.js
+++ public/scripts/themed_scripts/threed/main.js
@@ -16,7 +16,6 @@ export default class ThreeD extends Theme {
         const description = activeProject.querySelector('.project-info p');
         description === null || description === void 0 ? void 0 : description.addEventListener('animationend', () => {
             const color = getProminentColor(activeProject.querySelector('img'));
-            console.log(color);
             document.body.style.background = `rgb(${color.r},${color.g},${color.b})`;
             this.listeners.push({
                 element: document,
