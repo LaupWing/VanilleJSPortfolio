34383a5cc8f01a97d5b9fc856b7a576749228d2c
LaupWing <loc_strict@hotmail.com>
2020-06-07 16:54:48 +0200

    working home animation

--- public/scripts/themed_scripts/threed/Sections/Home/Home.js
+++ public/scripts/themed_scripts/threed/Sections/Home/Home.js
@@ -2,8 +2,14 @@ import Sections from '../Sections.js';
 export default class Home extends Sections {
     constructor(listeners, body) {
         super(listeners, body);
+        this.movingContainer = document.querySelector('div.content');
         this.init();
     }
     init() {
+        this.addListener({
+            element: document,
+            type: 'mousemove',
+            referenceFunction: this.move
+        });
     }
 }
--- src/themed_scripts/threed/Sections/Home/Home.ts
+++ src/themed_scripts/threed/Sections/Home/Home.ts
@@ -4,8 +4,14 @@ import Sections from '../Sections.js';
 export default class Home extends Sections{
     constructor(listeners:isListener[], body:HTMLBodyElement){
         super(listeners, body);
+        this.movingContainer = document.querySelector('div.content');
         this.init();
     }
     init(){
+        this.addListener({
+           element: document,
+           type: 'mousemove',
+           referenceFunction: this.move
+        });
     }
 }
