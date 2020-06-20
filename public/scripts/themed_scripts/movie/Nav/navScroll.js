76f1746a01ec1f966d91e15c6dbf940f8398a6f4
LaupWing <loc_strict@hotmail.com>
2020-06-20 18:52:17 +0200

    moving menu item to center works

--- public/scripts/themed_scripts/movie/Nav/navScroll.js
+++ public/scripts/themed_scripts/movie/Nav/navScroll.js
@@ -1,7 +1,20 @@
+let initialCoords = null;
 export default function navScroll(e) {
+    if (!initialCoords) {
+        initialCoords = Array
+            .from(document.querySelectorAll('nav li'))
+            .map(x => {
+            return {
+                element: x,
+                left: x.getBoundingClientRect().left
+            };
+        });
+    }
     const nav = document.querySelector('nav ul');
     const target = e.target;
+    const li = target.closest('li');
     const viewWidth = window.innerWidth;
-    const targetWidth = target.getBoundingClientRect().width;
-    nav.style.transform = `translateX(${(viewWidth / 2) - (targetWidth / 2)}px)`;
+    const { width } = li.getBoundingClientRect();
+    const left = initialCoords.find(x => x.element === li).left;
+    nav.style.transform = `translateX(${((viewWidth / 2) - (width / 2)) - left}px)`;
 }
--- src/themed_scripts/movie/Nav/navScroll.ts
+++ src/themed_scripts/movie/Nav/navScroll.ts
@@ -1,7 +1,26 @@
+let initialCoords:null|coords[]= null;
+interface coords{
+    element: HTMLElement,
+    left: number
+}
+
 export default function navScroll(e:MouseEvent){
+    if(!initialCoords){
+        initialCoords = Array
+            .from(document.querySelectorAll('nav li'))
+            .map(x=>{
+                return{
+                    element: x as HTMLElement,
+                    left: x.getBoundingClientRect().left
+                }
+            })
+    }
+
     const nav = document.querySelector('nav ul') as HTMLElement;
     const target = e.target as HTMLElement;
+    const li = target.closest('li') as HTMLLIElement;
     const viewWidth = window.innerWidth;
-    const targetWidth = target.getBoundingClientRect().width;
-    nav.style.transform = `translateX(${(viewWidth/2)-(targetWidth/2)}px)`;
+    const {width} = li.getBoundingClientRect();
+    const left = initialCoords.find(x=>x.element === li)!.left;
+    nav.style.transform = `translateX(${((viewWidth/2)-(width/2)) - left}px)`;
 }
